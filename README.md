290d690d517426019e1d55d43e67d79bd9596c90
Vipul-19 <72202638+Vipul-19@users.noreply.github.com>
2021-03-09 23:57:17 +0530

    docs added README

---
+++ README.md
@@ -0,0 +1,5 @@
+README.md
+    # student portfolio
+
+    Hey ! welcome to my portfolio . It's coming up tomorrow when i learn
+     HTML and CSS with pratap live on youtube
---
+++ vipul.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main()
+{
+    printf("ky mg vipul ky challa
+    biko pora ky mhantat--majet ka
+    jast tension nay ghevana, majana ravana khavana aitit rahavana--nai ka");
+}
